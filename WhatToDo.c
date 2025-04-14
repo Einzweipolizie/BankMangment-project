0569f6bd7b647a89b85f57fff844cd86f41b0f47
Eins <davidkan1911@gmail.com>
2025-04-14 20:28:59 +0300

    just casul savings

--- WhatToDo.c
+++ WhatToDo.c
@@ -9,6 +9,9 @@
 // there is prblem in acount creaintg function i need to find where to put getchar becuse i did somthing for now and it need to be fixed
 // i dont know why but my userDB have problems for exemple in chek balnce its not working with this need to cheek
 // when you delete transactions you need only what your recive into the acount you saved
-
+// need to chek the tests
+//
+// 
 
 // WHAT TO DO NOW!!:
+//
